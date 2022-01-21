504712c4bd4364432c919540e55d7fd3fbb32623
ann <davidgluttongk@outlook.com>
2022-01-21 11:57:30 +0800

    [feat](eg/foundation-example): add example for spring-boot-starter-test

--- api/src/test/java/example/mybaits_plus/SimpleTest.java
+++ api/src/test/java/example/mybaits_plus/SimpleTest.java
@@ -1,1 +1,16 @@
-//package example.mybaits_plus;
+package example.mybaits_plus;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SimpleTest {
+    @Test
+    void test() {
+        int a = 1;
+        int b = 1;
+
+        Assertions.assertEquals(a, b);
+    }
+}
